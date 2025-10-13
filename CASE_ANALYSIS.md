49606c08e4586edc7a1f534f12974a2126a5f24f
Andrew Jowe <andrewjowe@yahoo.com>
2025-10-13 16:23:35 -0700

    task 2 code almost done

--- CASE_ANALYSIS.md
+++ CASE_ANALYSIS.md
@@ -1,25 +1,38 @@
 # 1
 
-## No env noise
+euclidean metric
 
-VGG_FACE_2 MAP = 0.6463
+**Table 1.** Comparison of Mean Average Precision (mAP) for vggface2 and casia_webface under different conditions.
 
-CASIA_WEBFACE MAP = 0.3726
-
-## Gaussian blur
-VGG_FACE_2 MAP = 0.5575
-
-CASIA_WEBFACE MAP = 0.2677
-
-## Resizing
-VGG_FACE_2 MAP = 0.6278
-
-CASIA_WEBFACE MAP = 0.3678
-
-## Brightness adjustment
-VGG_FACE_2 MAP = 0.5506
-
-CASIA_WEBFACE MAP = 0.3561
+| Condition              | vggface2 mAP | casia_webface mAP |
+|------------------------|--------------|-------------------|
+| No env noise           | 0.6463       | 0.3726            |
+| Gaussian blur          | 0.5575       | 0.2677            |
+| Resizing               | 0.6278       | 0.3678            |
+| Brightness adjustment  | 0.5506       | 0.3561            |
 
 # 2
+VGG FACE, euclidean metric
+
+## Brute Force
+Time Add Identities (s):
+0.033447265625
+Mem footprint (GB):
+0.00432
+Time Query Probes (s/query):
+0.11519265174865723
+Max queries/s:
+8.681109296641022
+
+## HNSW
+Time Add Identities (s):
+0.3884561061859131
+Mem footprint (GB):
+0.004893197
+Time Query Probes (s/query):
+0.14606595039367676
+Max queries/s:
+6.846222526911997
+
+## LSH
 
---
+++ analysis/task2.py
@@ -0,0 +1,66 @@
+import os
+from typing import List
+import requests
+from analysis.task1 import query_probes, mean_ap
+
+GALLERY_DIR = os.path.join(
+    'multi_image_identities',
+    'multi_image_gallery'
+)
+
+PROBE_DIR = os.path.join(
+    'multi_image_identities',
+    'probe'
+)
+
+# List only subdirectories
+def get_identities_from(directory: str) -> List[str]:
+    return [
+        d for d in os.listdir(directory)
+        if os.path.isdir(os.path.join(directory, d))
+    ]
+
+def add_identities() -> float:
+    res = 0.0
+    for identity in get_identities_from(GALLERY_DIR):
+        identity_path = os.path.join(GALLERY_DIR, identity)
+        for file in os.listdir(identity_path):
+            image_path = os.path.join(identity_path, file)
+            response = requests.post("http://localhost:3000/add", files={"image": open(image_path, "rb")}, data={"name": file[:-4]})
+            res += response.json().get("build_time")
+
+    return res
+
+def time_query_probe(image_path: str, k: int = 5) -> float:
+    r = requests.post("http://localhost:3000/identify",
+                      files={"image": open(image_path, "rb")},
+                      data={"k": str(k)})
+    return r.json().get("search_time")
+
+def time_query_probes() -> float:
+    res = 0.0
+
+    for identity in get_identities_from(PROBE_DIR):
+        identity_path = os.path.join(PROBE_DIR, identity)
+        for file in os.listdir(identity_path):
+            image_path = os.path.join(identity_path, file)
+            res += time_query_probe(image_path, k=5)
+
+    return res
+
+def main():
+    print("Time Add Identities (s):")
+    print(add_identities())
+    print("Mem footprint (GB):")
+    r = requests.get("http://localhost:3000/index/memory")
+    print(r.json().get("memory_footprint_gb"))
+    print("Time Query Probes (s/query):")
+    search_time = time_query_probes()
+    print(search_time)
+    print("Max queries/s:")
+    print(1/search_time)
+    # print("mAP:")
+    # print(mean_ap(query_probes()))
+
+if __name__ == "__main__":
+    main()
