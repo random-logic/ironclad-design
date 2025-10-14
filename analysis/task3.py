3d2408f8e7fe958105b24efc6ac2bf7319733549
random <andrewjowe@yahoo.com>
2025-10-13 21:51:00 -0700

    more analysis

--- analysis/task3.py
+++ analysis/task3.py
@@ -6,12 +6,10 @@ from analysis.task2 import time_query_probes
 if __name__ == '__main__':
     add_identities()
 
-    '''
     res = np.array(query_probes(k=7))
 
     for k in range(1, 7):
         print(f"mAP for k = {k}:", mean_ap(res[:, :k]))
-    '''
 
     for k in range(1, 7):
         print(f"Time Query Probes k={k} (s/query):")
